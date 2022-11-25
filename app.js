96f6d3734ddd88989c57de75ff32b35caa22874b
Aleksey Kriuchkov <aleksejkriuchkov@gmail.com>
2022-11-25 15:09:44 +0300

    Another completed JS task

--- app.js
+++ app.js
@@ -890,3 +890,14 @@
 //     }
 //     return array
 // }
+
+// let arr = [4,6,2,1,9,63,-134,566]
+// var min = function(list){
+//     list.sort((a,b)=>a-b)
+//     return list[0];
+// }
+
+// var max = function(list){
+//     list.sort((a,b)=>a-b)
+//     return list[list.length-1];
+// }
